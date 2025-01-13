26b4cbd1b665aed479fc66767966f1d659959c86
Sumit Kumar <iteensumit@gmail.com>
2025-01-13 22:17:14 +0530

    window create_flutter_project created

--- create_flutter_project.bat
+++ create_flutter_project.bat
@@ -1,114 +1,110 @@
 @echo off
-
-REM Path to the template GitHub repository (URL)
 SET FLUTTER_TEMPLATE_REPO_URL=https://github.com/iosumit/com.flutter.template.git
 
-REM Arguments
+:: Arguments
 SET PROJECT_NAME=%1
 SET ORG_NAME=%2
 SET NEW_PROJECT_PATH=%3
 
-REM Set default organization name if not provided
-IF "%ORG_NAME%"=="" (
-    SET ORG_NAME=com
+:: Validate the project name (only lowercase letters allowed)
+echo %PROJECT_NAME%| findstr /R "[^a-z]" >nul
+IF %ERRORLEVEL% NEQ 1 (
+    echo Error: Project name must contain only lowercase letters and no special characters or numbers.
+    exit /b 1
 )
 
-REM Validate the project name (only lowercase letters allowed)
-FOR /F "delims=abcdefghijklmnopqrstuvwxyz" %%C IN ("%PROJECT_NAME%") DO (
-    ECHO Error: Project name must contain only lowercase letters and no special characters or numbers.
-    EXIT /B 1
-)
 
 IF "%PROJECT_NAME%"=="" (
-    ECHO Usage: create_flutter_project.bat <project_name> [organization_name] <new_project_path>
-    EXIT /B 1
+    echo Usage: create_flutter_project.bat ^<project_name^>
+    exit /b 1
 )
 
-REM Navigate to the specified project directory
-PUSHD "%NEW_PROJECT_PATH%" || EXIT /B 1
+:: Set default organization name if not provided
+IF "%ORG_NAME%"=="" SET ORG_NAME=com
 
-REM Create the new Flutter project
-ECHO Creating Flutter project "%PROJECT_NAME%" with the organization "com.%PROJECT_NAME%"
-flutter create --org "%ORG_NAME%" "%PROJECT_NAME%"
-IF ERRORLEVEL 1 (
-    ECHO Failed to create the Flutter project. Exiting...
-    EXIT /B 1
+:: Navigate to the specified project directory
+cd "%NEW_PROJECT_PATH%" || exit /b
+
+:: Create the new Flutter project
+echo Creating Flutter project: "%PROJECT_NAME%" with the bundle-id: "%ORG_NAME%.%PROJECT_NAME%"
+powershell -Command "flutter create --org %ORG_NAME% %PROJECT_NAME%"
+
+echo "Project Created"
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to create the Flutter project. Exiting...
+    exit /b 1
 )
-
-REM Navigate to the new project directory
+:: Navigate to the new project directory
 IF "%NEW_PROJECT_PATH%"=="" (
-    PUSHD "%PROJECT_NAME%"
+    cd "%PROJECT_NAME%" || exit /b
 ) ELSE (
-    PUSHD "%NEW_PROJECT_PATH%\%PROJECT_NAME%"
+    cd "%NEW_PROJECT_PATH%/%PROJECT_NAME%" || exit /b
 )
 
-REM Clone the template repository into the new project directory
-ECHO Cloning template files from GitHub...
-git clone "%FLUTTER_TEMPLATE_REPO_URL%" .temp_template
+:: Clone the template repository into the new project directory
+echo Cloning template files from GitHub...
+git clone %FLUTTER_TEMPLATE_REPO_URL% .temp_template
+:: Remove the template's .git directory to avoid conflicts with the new project
+rmdir /s /q .temp_template\.git
 
-REM Remove the template's .git directory to avoid conflicts with the new project
-RMDIR /S /Q .temp_template\.git
+mkdir assets
+mkdir .vscode
 
-REM Ensure required directories exist
-MKDIR assets
-MKDIR .vscode
+:: Copy the template files into the new project directory, replacing the lib and assets directories
+echo Copying template files into the new project...
+xcopy /e /i /Y .temp_template\lib\* lib\
+xcopy /e /i /Y .temp_template\assets\* assets\
+xcopy /e /i /Y .temp_template\.vscode\* .vscode\
+:: Copy individual files (l10n.yaml and gen)
+echo f | xcopy /f .temp_template\l10n.yaml l10n.yaml /Y
+echo f | xcopy /f .temp_template\gen gen /Y /E
+echo f | xcopy /f .temp_template\gen.bat gen.bat /Y /E
+:: Make the gen file executable 
 
-REM Copy the template files into the new project directory
-ECHO Copying template files into the new project...
-XCOPY /E /I /Q .temp_template\lib lib
-XCOPY /E /I /Q .temp_template\assets assets
-XCOPY /E /I /Q .temp_template\.vscode .vscode
-COPY /Y .temp_template\l10n.yaml l10n.yaml
-XCOPY /E /I /Q .temp_template\gen gen
-attrib +x gen
+:: Clean up the temporary cloned repository
+rmdir /s /q .temp_template
 
-REM Clean up the temporary cloned repository
-RMDIR /S /Q .temp_template
-
-REM Replace placeholder variables (like flutter_template) with the project name
-ECHO Customizing files with project name "%PROJECT_NAME%"...
-FOR /R %%F IN (*.dart *.yaml) DO (
-    FINDSTR /I "flutter_template" "%%F" >NUL
-    IF ERRORLEVEL 0 (
-        powershell -Command "(Get-Content -Path '%%F') -replace 'flutter_template', '%PROJECT_NAME%' | Set-Content -Path '%%F'"
-        ECHO Updated %%F
+:: Replace placeholder variables (like flutter_template) with the project name
+echo Customizing files with project name "%PROJECT_NAME%"...
+FOR /R %%f IN (*.dart *.yaml) DO (
+    findstr /C:"flutter_template" "%%f" >nul
+    IF %ERRORLEVEL% EQU 0 (
+        powershell -Command "(Get-Content '%%f') -replace 'flutter_template', '%PROJECT_NAME%' | Set-Content '%%f'"
+        echo Updated %%f
     )
 )
 
-REM Update pubspec.yaml file to add necessary configurations
+:: Update pubspec.yaml file to add necessary configurations
 SET PUBSPEC_FILE=pubspec.yaml
-ECHO Updating pubspec.yaml...
+echo Updating pubspec.yaml...
 
-FINDSTR /B /C:"flutter:" "%PUBSPEC_FILE%" >NUL
-IF ERRORLEVEL 1 (
-    ECHO flutter:
-    ECHO   generate: true
-    ECHO   uses-material-design: true
-    ECHO
-    ECHO   assets:
-    ECHO     - assets/
-    ECHO     - assets/images/
-    ECHO     - assets/icons/
-) >> "%PUBSPEC_FILE%"
+findstr /C:"uses-material-design: true" %PUBSPEC_FILE% >nul
+IF %ERRORLEVEL% EQU 0 (
+    powershell -Command "$path = '%PUBSPEC_FILE%'; $content = [System.Collections.Generic.List[System.String]](Get-Content $path); $modified = $false; for ($i = 0; $i -lt $content.Count; $i++) { $line = $content[$i]; if ($line -match 'uses-material-design: true') { $content.Insert($i+1, '  generate: true'); $content.Insert($i+2, '  assets:'); $content.Insert($i+3, '    - assets/'); $content.Insert($i+4, '    - assets/images/'); $content.Insert($i+5, '    - assets/icons/'); $modified = $true } }; if ($modified) { $content | Set-Content $path } else { Write-Host 'No changes made to the file' }"
 ) ELSE (
-    powershell -Command "(Get-Content -Path '%PUBSPEC_FILE%') -replace '^flutter:', 'flutter:`r`n  generate: true`r`n  assets:`r`n    - assets/`r`n    - assets/images/`r`n    - assets/icons/' | Set-Content -Path '%PUBSPEC_FILE%'"
+    echo ^"flutter^": >> %PUBSPEC_FILE%
+    echo ^  generate: true >> %PUBSPEC_FILE%
+    echo ^  uses-material-design: true >> %PUBSPEC_FILE%
+    echo. >> %PUBSPEC_FILE%
+    echo ^  assets:^ >> %PUBSPEC_FILE%
+    echo     - assets/ >> %PUBSPEC_FILE%
+    echo     - assets/images/ >> %PUBSPEC_FILE%
+    echo     - assets/icons/ >> %PUBSPEC_FILE%
 )
 
-REM Add dependencies
-flutter pub add flutter_localizations --sdk=flutter
-flutter pub add intl:any
-flutter pub add provider
-flutter pub add shared_preferences
-flutter pub add http
+:: Add necessary packages to pubspec.yaml
+powershell -Command "flutter pub add flutter_localizations --sdk=flutter"
+powershell -Command "flutter pub add intl:any"
+powershell -Command "flutter pub add provider"
+powershell -Command "flutter pub add shared_preferences"
+powershell -Command "flutter pub add http"
 
-REM Run the generator script
-CALL gen
+:: Add generated files to .gitignore
+echo /lib/generated/ >> .gitignore
 
-ECHO. >> .gitignore
-ECHO /lib/generated/ >> .gitignore
+:: Run gen 
+gen
 
-REM Final message
-ECHO Flutter project "%PROJECT_NAME%" created, customized, and template files copied successfully!
-
-REM Return to the original directory
-POPD
+:: Final message
+echo Flutter project "%PROJECT_NAME%" created, customized, and template files copied successfully!
